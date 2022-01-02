d3816339568085f4d0375dd30fceee24c7ffdbb8
geng peng <gp1478963@outlook.com>
2022-01-02 21:46:35 +0800

    first

--- gTest/test_about.cpp
+++ gTest/test_about.cpp
@@ -2,4 +2,21 @@
 // Created by gp147 on 2022/1/2.
 //
 
-#include "test_about.h"
+#include "../unix_interface_test.h"
+#include <gtest/gtest.h>
+#include <vector>
+#include <numeric>
+/**
+ * @brief 测试单个函数
+ */
+TEST(TestS, AddFunc){
+  std::vector<int> nums{1,2,3,4,5};
+  auto sum = std::accumulate(nums.begin(), nums.end(), 0);
+  EXPECT_EQ(sum, 15);
+}
+
+int
+main(int argc, char* argv[]){
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
